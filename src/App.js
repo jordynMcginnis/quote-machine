d2231fb607370ea35ab7dea08817eecb2f8c50c7
jordynmcginnis <jordynm4@gmail.com>
2017-04-14 19:56:53 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+var api = require('../utils/api');
 
 class NextResults extends Component {
+
   render () {
     return (
-      <button className = 'next-button'> Next Quote > </button>
+      <div>
+        <div className='both-buttons'>
+          <button className='add-button' onClick={this.props.addItem}>Add to list +</button>
+          <button
+            onClick={this.props.updateQuote}
+            className='next-button'>
+              Next Quote >
+          </button>
+        </div>
+
+        <div className='fav-quote-title'>
+          <h1> Favorite Quotes </h1>
+          <div className='list-item'>
+            {this.props.listItem.length !== 0 ?
+              <ul>
+                {this.props.listItem.map(function(item){
+                return <li className='li'key={item}>{item}</li>
+              })}
+                <button className='delete'onClick={this.props.deleteList}> Delete All Quotes </button>
+              </ul>
+            : <p> no favorite quotes</p>}
+          </div>
+
+        </div>
+
+      </div>
+    )
+  }
+}
+class QuotesResults extends Component{
+  render () {
+    return (
+      <div className='list-items'> <h1> {this.props.quote} </h1> </div>
     )
   }
 }
 class Results extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      quote: null,
+      list: []
+    }
+    this.updateQuote = this.updateQuote.bind(this);
+    this.fetchQuote = this.fetchQuote.bind(this);
+    this.addToList = this.addToList.bind(this);
+    this.deleteList = this.deleteList.bind(this);
+  }
+  updateQuote (item){
+    this.setState(function(){
+      return {
+        quote: null
+      }
+    });
+    this.fetchQuote()
+  }
+  fetchQuote() {
+    api.getQuote()
+      .then(function(response){
+        console.log(response)
+        this.setState(function(){
+          return {
+            quote: response.quoteText
+          }
+        })
+      }.bind(this))
+  }
+  addToList (item) {
+    var newItem = this.state.quote
+    this.setState(function(){
+      return {
+        list: this.state.list.concat([newItem])
+      }
+    })
+    this.fetchQuote()
+  }
+  deleteList () {
+    this.setState(function(){
+      return {
+        list: []
+      }
+    })
+  }
+  componentDidMount() {
+    this.fetchQuote();
+  }
   render () {
     return (
-      <h1>quote will go here</h1>
+      <div className='main'>
+        <div>{ !this.state.quote ? <p> Loading</p> : <QuotesResults quote={this.state.quote}/>} </div>
+        <NextResults
+          updateQuote={this.updateQuote}
+          listItem={this.state.list}
+          addItem={this.addToList}
+          deleteList={this.deleteList}/>
+      </div>
     )
   }
 }
@@ -23,9 +111,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2><Results /></h2>
+          <Results />
         </div>
-          <NextResults />
       </div>
     );
   }
